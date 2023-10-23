fd9b4cd69d97e937453aa844223d24b57246f5f4
dansoaresfarias <dansoaresfarias@gmail.com>
2023-10-23 11:34:01 -0300

    tysprit aula 2

---
+++ TS/expW3C/figuras.ts
@@ -0,0 +1,39 @@
+// Try creating a new interface using it below
+interface iRectangle {
+    height?: number;
+    width?: number;
+    area(): number;
+}
+
+class Rectangle implements iRectangle {
+    private _height: number;
+    private _width: number;
+    constructor(height: number, width: number) {
+        this._height = height;
+        this._width = width;
+    }
+
+    area(): number {
+        return this._height * this._width;
+    }
+}
+
+class Square implements iRectangle {
+    constructor(
+        public height: number) {
+        this.height = height;
+    }
+
+    area(): number {
+        return this.height * this.height;
+    }
+}
+
+let ret = new Rectangle(10, 20);
+console.log(ret);
+//console.log(ret._height);
+console.log(ret.area());
+
+let quad = new Square(10);
+console.log(quad);
+console.log(quad.area());
